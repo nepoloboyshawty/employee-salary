0b78a2f82a7f34c162dc946e096cc51f5fff74be
Sergey Astakhov <sergeyastakhov24@gmail.com>
2025-09-24 20:42:20 +0300

    add modules

---
+++ modules/classes.py
@@ -0,0 +1,19 @@
+class Animals:
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+        self._sage = age / 2
+
+    @staticmethod
+    def animal_sound():
+        return "Нет звука"
+
+
+class Dog(Animals):
+    @property
+    def info(self):
+        return f"{self.name}, {self.age}, {self._sage}"
+
+    @staticmethod
+    def animal_sound():
+        return "Гав-гав"
